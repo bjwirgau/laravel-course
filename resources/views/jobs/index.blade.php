34ea1cf14bbf08a07b69f5b050bb5e0f828e39eb
Brandon Wirgau <brandonjoelwirgau@gmail.com>
2025-12-17 15:53:37 -0700

    3.25 Loop Directives

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -7,14 +7,22 @@
 </head>
 <body>
   <h1>{{$title}}</h1>
-  @if(!empty($jobs))
   <ul>
-    @foreach ($jobs as $job)
-      <li>{{$job}}</li>
-    @endforeach
+    @forelse ($jobs as $job)
+      {{-- @if($job == 'Data Scientist')
+        @continue
+      @endif --}}
+      <li>{{$loop->index}} - {{$job}}</li>
+      {{-- <li>{{$loop->iteration}} - {{$job}}</li> --}}
+      {{-- <li>{{$loop->remaining}} - {{$job}}</li> --}}
+      {{-- <li>{{$loop->count}} - {{$job}}</li> --}}
+      {{-- <li>{{$loop->first}} - {{$job}}</li> --}}
+      {{-- <li>{{$loop->last}} - {{$job}}</li> --}}
+      {{-- <li>{{$loop->even}} - {{$job}}</li>
+      <li>{{$loop->odd}} - {{$job}}</li> --}}
+    @empty
+      <li>No jobs available at the moment.</li>
+    @endforelse
   </ul>
-  @else
-    <p>No jobs available at the moment.</p>
-  @endif
 </body>
 </html>
