bbba1339152c438820abec627774c9c4bf09b281
Brandon Wirgau <brandonjoelwirgau@gmail.com>
2025-12-17 01:06:29 -0700

    3.19 Response Helper

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 
 Route::get('/', function () {
@@ -11,23 +12,24 @@ Route::get('/jobs', function() {
     return '<h1>Available Jobs</h1>';
 })->name('jobs');
 
-Route::get('/test', function (Request $request) {
-    return [
-        'method' => $request->method(),
-        'url' => $request->url(),
-        'path' => $request->path(),
-        'fullUrl' => $request->fullUrl(),
-        'ip' => $request->ip(),
-        'userAgent' => $request->userAgent(),
-        'header' => $request->header(),
-    ];
+Route::get('/test', function () {
+    return response('<h1>Hello World!</h1>', 200)->header('Content-Type', 'text/html');
 });
 
-Route::get('/users', function(Request $request) {
-    // return $request->query('name'); // get single query param
-    // return $request->only(['name', 'age']); // get specified query params
-    // return $request->all(); // get all query params
-    // return $request->has('name'); // returns a boolean value wether query param exists or not
-    // return $request->input('name', 'Default Name'); // this works both with URL query params and form input fields. Can return default value
-    // return $request->except(['name']); // Returns all query param values except the specified param
+Route::get('/json', function() {
+    return response()->json(['name' => 'John Doe'])->cookie('name', 'John Doe');;
+});
+
+Route::get('/read-cookie', function(Request $request) {
+    $cookieValue = $request->cookie('name');
+    
+    return response()->json(['cookie' => $cookieValue]);
+});
+
+Route::get('/download', function() {
+    return response()->download(public_path('favicon.ico'));
+});
+
+Route::get('/notfound', function () {
+    return new Response('Not Found', 404);
 });
