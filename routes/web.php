47403bbb51d421bb92ee5bea788935245fb3f1b3
Brandon Wirgau <brandonjoelwirgau@gmail.com>
2025-12-17 13:59:04 -0700

    3.23 Creating Views and Passing Data

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Response;
-use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/jobs', function() {
-    return '<h1>Available Jobs</h1>';
+    $title = 'Available Jobs';
+    $jobs = [
+        'Software Engineer',
+        'Data Scientist',
+        'Product Manager',
+        'UX Designer'
+    ];
+
+    // Method 1
+    // return view('jobs.index', [
+    //     'title' => 'Available Jobs'
+    // ]);
+
+    // Method 2
+    // return view('jobs.index')
+    //     ->with('title', $title)
+    //     ->with('jobs', $jobs);
+
+    // Method 3
+    return view('jobs.index', compact('title', 'jobs'));
+
 })->name('jobs');
 
-Route::get('/test', function () {
-    return response('<h1>Hello World!</h1>', 200)->header('Content-Type', 'text/html');
-});
-
-Route::get('/json', function() {
-    return response()->json(['name' => 'John Doe'])->cookie('name', 'John Doe');;
-});
-
-Route::get('/read-cookie', function(Request $request) {
-    $cookieValue = $request->cookie('name');
-    
-    return response()->json(['cookie' => $cookieValue]);
-});
-
-Route::get('/download', function() {
-    return response()->download(public_path('favicon.ico'));
-});
-
-Route::get('/notfound', function () {
-    return new Response('Not Found', 404);
-});
+Route::get('/jobs/create', function() {
+    return view('jobs.create');
+})->name('jobs.create');
