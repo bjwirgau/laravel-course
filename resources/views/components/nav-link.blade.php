4092f872693798f3b6bc4a51831e8c2ce2190197
Brandon Wirgau <brandonjoelwirgau@gmail.com>
2025-12-19 09:57:23 -0700

    5.38 Component Attributes & Props

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,12 @@
+@props([
+    'url' => '/',
+    'active' => false,
+    'icon' => null,
+])
+
+<a href="{{ $url }}" class="text-white hover:underline py-2 {{$active ? 'font-bold text-yellow-500' : ''}}">
+    @if($icon)
+        <i class="fa fa-{{$icon}} mr-1"></i>
+    @endif
+    {{ $slot }}
+</a>
