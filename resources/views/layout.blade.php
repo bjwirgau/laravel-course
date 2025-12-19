6e49213b7764dd3031239940fbd68e17c2a5b9d0
Brandon Wirgau <brandonjoelwirgau@gmail.com>
2025-12-19 13:38:57 -0700

    5.42 Hero Component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,19 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  @vite('resources/css/app.css')
-  <title>{{ $title ?? 'Workopia | Find and List Jobs' }}</title>
-</head>
-<body class="bg-gray-100">
-  <x-header />
-  <main class="container mx-auto p-4 mt-4">
-    {{$slot}}
-  </main>
 
-  <script src="{{ asset('js/script.js') }}"></script>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <title>{{ $title ?? 'Workopia | Find and List Jobs' }}</title>
+</head>
+
+<body class="bg-gray-100">
+    <x-header />
+    @if (request()->is('/'))
+        <x-hero />
+    @endif
+    <main class="container mx-auto p-4 mt-4">
+        {{ $slot }}
+    </main>
+
+    <script src="{{ asset('js/script.js') }}"></script>
 </body>
+
 </html>
