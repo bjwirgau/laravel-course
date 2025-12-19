5d39c788b1339ba719dcd577858252999c79421e
Brandon Wirgau <brandonjoelwirgau@gmail.com>
2025-12-19 13:58:28 -0700

    5.43 Top And Bottom Banners

--- resources/views/components/top-banner.blade.php
+++ resources/views/components/top-banner.blade.php
@@ -1,3 +1,15 @@
-<div>
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
-</div>
+@props([
+    'heading' => 'Unlock Your Career Potential',
+    'subheading' => 'Discover the perfect job opportunity for you.'
+])
+
+<section class="bg-blue-900 text-white py-6 text-center">
+    <div class="container mx-auto">
+        <h2 class="text-3xl font-semibold">
+            {{$heading}}
+        </h2>
+        <p class="text-lg mt-2">
+            {{$subheading}}
+        </p>
+    </div>
+</section>
