9112b2ee10a82b3046915423081db75150885eda
Brandon Wirgau <brandonjoelwirgau@gmail.com>
2025-12-19 17:34:54 -0700

    7.56 Model Binding

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,4 +1,4 @@
 <x-layout>
-  <h1>Job Details</h1>
-  <p>Details for job with ID: {{$id}}</p>
+  <h1 class="text-2xl font-bold mb-4">{{$job->title}}</h1>
+  <p>{{$job->description}}</p>
 </x-layout>
