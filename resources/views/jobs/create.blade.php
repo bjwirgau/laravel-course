95902a6b6f866df97d38881feeff085779d48a6d
Brandon Wirgau <brandonjoelwirgau@gmail.com>
2025-12-19 17:41:09 -0700

    7.57 Create Job List

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -3,8 +3,8 @@
   <h1>Create New Job</h1>
   <form action="/jobs" method="POST">
     @csrf
-    <input type="text" name="title" placeholder="Job Title">
-    <input type="text" name="description" placeholder="Job Description">
+    <input class="bg-white" type="text" name="title" placeholder="Job Title">
+    <input class="bg-white" type="text" name="description" placeholder="Job Description">
     <button type="submit">Create Job</button>
   </form>
 </x-layout>
