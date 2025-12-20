aa2117f082ea97995ec4fc6a55b9a6f468ac496c
Brandon Wirgau <brandonjoelwirgau@gmail.com>
2025-12-19 17:13:02 -0700

    7.54 Fetching Data & Elequent ORM

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
-class Job 
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
 {
-    public static function all(): array {
-        return [
-            ['id' => 1, 'title' => 'Software Engineer', 'description' => 'We are looking for a skilled software engineer to join our team.'],
-            ['id' => 2, 'title' => 'Data Analyst', 'description' => 'If you have a passion for data and analytics, we have an opening for you.'],
-            ['id' => 3, 'title' => 'Marketing Specialist', 'description' => 'Join our marketing team and help us grow our brand.']
-        ];
-    }
+    protected $table = 'job_listings';
+
+    protected $fillable = [
+        'title', 
+        'description'
+    ];
+
 }
