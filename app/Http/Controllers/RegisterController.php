1c3d33f0f6102fc78c2be8ee4b9706b0ce79d3a5
Brandon Wirgau <brandonjoelwirgau@gmail.com>
2025-12-23 10:11:01 -0700

    9.85 Register New User

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -16,5 +18,25 @@ class RegisterController extends Controller
     public function register(): View
     {
         return view('auth.register');
+    }
+
+        /**
+     * @desc Show register form
+     * @route GET /register
+     * @return View
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        $user = User::create($validatedData);
+
+        return redirect()->route('login')->with('success', 'Registration successful!');
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,19 @@
 <x-layout>
-  <h1>Register</h1>
+    <div class="bg-white rounded-lg shadow-md w-full md:max-w-2xl mx-auto mt-12 p-8 py-12">
+        <h2 class="text-4xl text-center font-bold mb-4">Register</h2>
+        <form method="POST" action="{{ route('register.store') }}">
+            @csrf
+            <x-inputs.text id="name" name="name" label="Full Name" placeholder="Full Name" />
+            <x-inputs.text id="email" name="email" label="Email" type="email" placeholder="Email Address" />
+            <x-inputs.text id="password" name="password" label="Password" type="password" placeholder="Password" />
+            <x-inputs.text id="password_confirmation" name="password_confirmation" label="Confirm Password"
+                type="password" placeholder="Confirm Password" />
+
+            <button type="submit"
+                class="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none">Register</button>
+
+            <p class="mt-4 text-center text-gray-500">Already have an account? <a class="text-blue-900"
+                    href="{{ route('login') }}">Login</a></p>
+        </form>
+    </div>
 </x-layout>
